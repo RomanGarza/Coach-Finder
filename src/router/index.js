37689d41042a0ec98accd80bd2b302beb0fd42c7
Roman Garza <roman_garza@protonmail.com>
2022-10-03 11:39:49 -0700

    Added views & NavBar

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
+import CoachesList from "../views/Coaches/CoachesList.vue";
+import CoachDetails from "../views/Coaches/CoachDetails.vue";
+import CoachRegistration from "../views/Coaches/CoachRegistration.vue";
+import RequestsReceived from "../views/Requests/RequestsReceived.vue";
+import NotFound from "../views/NotFound.vue";
+import ContactCoach from "../views/Requests/ContactCoach.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      redirect: "/coaches",
+      redirect: "/coaches", 
     },
     {
       path: "/coaches",
       name: "Coaches",
-      component: null,
+      component: CoachesList,
     },
     {
       path: "/coach/:id",
       name: "CoachId",
-      component: null,
+      component: CoachDetails,
       children: [
         {
           path: "/contact",
-          component: null,
+          component: ContactCoach ,
         },
       ],
     },
     {
       path: "/register",
       name: "Register",
-      component: null,
+      component: CoachRegistration,
     },
     {
       path: "/requests",
       name: "Requests",
-      component: null,
+      component: RequestsReceived,
     },
     {
       path: "/:notFound(.*)",
-      component: null,
+      component: NotFound,
     },
   ],
 });
