827e4bd3a2a11873ce6c07655460bc191b809bfc
Roman Garza <roman_garza@protonmail.com>
2022-10-03 10:51:33 -0700

    Router configured

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      redirect: "/coaches",
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/coaches",
+      name: "Coaches",
+      component: null,
+    },
+    {
+      path: "/coach/:id",
+      name: "CoachId",
+      component: null,
+      children: [
+        {
+          path: "/contact",
+          component: null,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: null,
+    },
+    {
+      path: "/requests",
+      name: "Requests",
+      component: null,
+    },
+    {
+      path: "/:notFound(.*)",
+      component: null,
+    },
+  ],
+});
 
-export default router
+export default router;
