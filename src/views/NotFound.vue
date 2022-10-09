edfb0845d5e792dbb3244ba177689c17dc1756a0
Roman Garza <roman.garza@cascadefintech.com>
2022-10-08 17:25:24 -0700

    contact page, notFound page added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+    <base-card>
+    <h2>Page not found.</h2>
+    <p>This page could not be found. Return back to main page <router-link class="font-bold text-indigo-700" to="/coaches">here.</router-link></p>
+    </base-card>
+</section>
+</template>
--- src/views/Requests/RequestsReceived.vue
+++ src/views/Requests/RequestsReceived.vue
@@ -1,3 +1,47 @@
 <template>
-    hello
+    <section>
+        <base-card>
+            <header>
+                <h2>Requests Received</h2>
+            </header>
+            <ul></ul>
+            <h3>No requests received yet.</h3>
+        </base-card>
+    </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        receivedRequests() {
+            return this.$store.getters['requests/requests']
+        },
+        hasRequests() {
+            return this.$store.getters['requests/hasRequests']
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+header {
+    text-align: center;
+}
+
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+}
+
+h3 {
+    text-align: center;
+}
+</style>
